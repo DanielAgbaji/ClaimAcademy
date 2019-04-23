7dd80cded6910b47e3ed4a1a37c42dbb4252dc60
Daniel <dagbaji@g.emporia.edu>
2019-04-23 16:20:49 -0500

    Some updates!

---
+++ ClassesAndObjects/src/Box.java
@@ -0,0 +1,45 @@
+/*
+ *  
+rite a "Box" class which has the following properties:
+cost of material per square inch, 
+height in inches, 
+length in inches, 
+depth in inches. 
+ 
+Provide methods for calculating the "Volume", "SurfaceArea" and "Cost". 
+ 
+Write a program to use the class by creating a Box object and
+asking the user for each of the properties, setting these properties 
+for the box and printing out the Volume, Surface Area and Cost of the box.
+ */
+public class Box {
+	
+	//Properties of Box Class
+	
+	private double height;
+	private double length;
+	private double depth;
+	private double costPerSquare;
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static double boxVolume() {
+		
+		
+	}
+	public static double boxSurface() {
+		
+		
+	}
+	public static double boxCost() {
+		
+		
+	}
+
+}
---
+++ ClassesAndObjects/src/Date.java
@@ -0,0 +1,11 @@
+/*
+ * 
+ */
+public class Date {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
