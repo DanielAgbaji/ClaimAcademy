421553676325fa32f0eec8275b39fedf3513ade5
Daniel <dagbaji@g.emporia.edu>
2019-04-24 11:20:24 -0500

    Another update!

--- TuesdayLabTest/src/com/claim/Patron.java
+++ TuesdayLabTest/src/com/claim/Patron.java
@@ -1,5 +1,15 @@
 package com.claim;
 
 public class Patron {
+	
+	private String PatronFirstName= "";
+	private String PatronLastName= "";
+	private String PatronAddress= "";
+	
+	private String PatronLibraryName= "";
+	
+	private String PatronLibraryCardNumber = "";
+	
+	
 
 }
