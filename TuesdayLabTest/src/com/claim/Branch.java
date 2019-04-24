b462e8b38e78242043d535b22a9dd4408ea41f65
Daniel <dagbaji@g.emporia.edu>
2019-04-24 16:16:46 -0500

    Changes made to the branch website

--- TuesdayLabTest/src/com/claim/Branch.java
+++ TuesdayLabTest/src/com/claim/Branch.java
@@ -13,6 +13,39 @@ public class Branch {
 	}
 	public Branch(String branchName, long branchTelephone, 
 			String branchAddress, String branchEmail, String branchWebsite ) {
+		this.branchName = branchName;
+		this.branchTelephone = branchTelephone;
+		this.branchAddress = branchAddress;
+		this.branchEmail = branchEmail;
+		this.branchWebsite = branchWebsite;
+	
+	}
+	public String getBranchName() {
+		return this.branchName;
+	}
+	public void setBranchName (String branchName) {
+		this.branchName=branchName;
 		
 	}
+	public long getBranchTelephone() {
+		return this.branchTelephone;
+	}
+	public void setBranchTelephone(long branchTelephone) {
+		this.branchTelephone= branchTelephone;
+		
+	}
+	public String getBranchEmail() {
+		return this.branchName;
+	}
+	public void setBranchEmail(String branchEmail) {
+		this.branchEmail= branchEmail;
+		
+	}
+	public String getBranchWebsit() {
+		return this.branchWebsite;
+	}
+	public void setBranchWebsite(String branchWebsite) {
+		this.branchWebsite=branchWebsite;
+	}
+	
 }
